726d1fcb5dd73280370be6d128b3fec2a71cd589
mochamad.rizky <mochamadrizky.dev@gmail.com>
2021-01-18 19:23:55 +0700

    [UPDATE] DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call([
+            CatalogSeeder::class,
+            LevelSeeder::class
+        ]);
         // \App\Models\User::factory(10)->create();
     }
 }
