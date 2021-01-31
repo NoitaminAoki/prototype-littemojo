aac318cb9b9e753a6b7a6de5ae87e7b930b37ccb
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-01-31 17:40:43 +0700

    [UPDATE] refactoring partner seeder and routing

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admins\DashboardController as AdminDashboard;
 use App\Http\Controllers\Admins\AuthController as AdminAuthController;
 use App\Http\Controllers\Admins\Master\{
-    CatalogController, CatalogTopicController, LevelController
+    CatalogController,
+    CatalogTopicController,
+    LevelController
 };
 use App\Http\Controllers\Admins\Manage\UserController;
 use App\Http\Controllers\Partners\{
@@ -12,31 +14,16 @@ use App\Http\Controllers\Partners\{
     AuthController as PartnerAuthController,
     CourseController as PartnerCourseController,
 };
+use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::view('/', 'welcome');
+Route::view('dashboard', 'dashboard')->middleware(['auth:sanctum', 'verified'])->name('dashboard');
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
-
-
-
-Route::group(['middleware' => 'auth:admin', 'prefix' => 'admin/management', 'as' => 'admin.'], function () {
-    Route::get('/dashboard', [AdminDashboard::class, 'index'])->name('dashboard'); 
-    Route::get('user/{id}/update_status', [UserController::class, 'update_status'])->name('update_status');   
+Route::middleware('auth:admin')->prefix('admin/management')->name('admin.')->group(function () {
+    Route::get('/dashboard', [AdminDashboard::class, 'index'])->name('dashboard');
+    Route::get('user/{id}/update_status', [UserController::class, 'update_status'])->name(
+        'update_status'
+    );
     Route::resources([
         'catalog'       => CatalogController::class,
         'catalog_topic' => CatalogTopicController::class,
@@ -46,41 +33,34 @@ Route::group(['middleware' => 'auth:admin', 'prefix' => 'admin/management', 'as'
     Route::post('/logout', [AdminAuthController::class, 'logout'])->name('logout');
 });
 
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/register', [AdminAuthController::class, 'registerForm'])->name('register.form');
     Route::post('/register', [AdminAuthController::class, 'register'])->name('register');
-
-    Route::middleware('guest:admin')->get('/login', [AdminAuthController::class, 'loginForm'])->name('login.form');
-    Route::middleware('guest:admin')->post('/login', [AdminAuthController::class, 'login'])->name('login');
-
-});
-
-
-Route::group(['middleware' => 'auth:partner', 'prefix' => 'partner/management', 'as' => 'partner.'], function () {
-    Route::get('/dashboard', [PartnerDashboard::class, 'index'])->name('dashboard');
-
-    Route::post('/logout', [PartnerAuthController::class, 'logout'])->name('logout');
-
-    Route::group(['as' => 'manage.' ], function () {
-        Route::resource('course', PartnerCourseController::class);
+    Route::middleware(['gues:admin'])->group(function () {
+        Route::get('login', [AdminAuthController::class, 'loginForm'])->name('login.form');
+        Route::post('login', AdminAuthController::class, 'login')->name('login');
     });
 });
 
-Route::group(['prefix' => 'partner', 'as' => 'partner.'], function () {
+Route::middleware(['auth:partner'])->prefix('partner/management')->name('partner.')
+    ->group(function () {
+        Route::get('/dashboard', [PartnerDashboard::class, 'index'])->name('dashboard');
+        Route::post('/logout', [PartnerAuthController::class, 'logout'])->name('logout');
+        Route::name('manage.')->group(function () {
+            Route::resource('course', PartnerCourseController::class);
+        });
+    });
+
+Route::prefix('partner')->name('partner.')->group(function () {
     Route::get('/register', [PartnerAuthController::class, 'registerForm'])->name('register.form');
     Route::post('/register', [PartnerAuthController::class, 'register'])->name('register');
-
-    Route::middleware('guest:partner')->get('/login', [PartnerAuthController::class, 'loginForm'])->name('login.form');
-    Route::middleware('guest:partner')->post('/login', [PartnerAuthController::class, 'login'])->name('login');
-
+    Route::middleware(['guest:partner'])->group(function () {
+        Route::get('login', [PartnerAuthController::class, 'loginForm'])->name('login.form');
+        Route::post('login', [PartnerAuthController::class, 'login'])->name('login');
+    });
 });
 
-
-
-
-
-
-
+// auth login
 
 Route::get('pass-login', function () {
     $credentials = ['email' => 's2.DanielAoki@gmail.com', 'password' => 'Password123'];
