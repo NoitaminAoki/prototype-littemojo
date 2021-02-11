6ea5c56838ff13e76f86da51f5dc9cd222d54b0b
Sendii <sendidianhadiwijaya@gmail.com>
2021-02-11 19:52:24 +0700

    Add message alert if user deactive

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,12 +2,23 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
+
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                @if(\Auth::user()->status == 'D')
+                <div role="alert mb-3">
+                    <div class="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+                        Danger
+                    </div>
+                    <div class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
+                        <p>Your account has been deactivated, please contact the admin for details.</p>
+                    </div>
+                </div>
+                @endif
                 <x-jet-welcome />
             </div>
         </div>
